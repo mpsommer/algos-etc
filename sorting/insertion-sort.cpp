6ffe3e851c5025239665d117a65fce57f415a0af
Michael Sommer <mps@mindsharemed.com>
2018-09-10 08:37:08 -0700

    Implememented insertion sort.

--- sorting/insertion-sort.cpp
+++ sorting/insertion-sort.cpp
@@ -4,15 +4,15 @@
 #include <math.h>
 
 void insertionSort(int arr[], int n) {
-	int i, key, j;
+	int i, j, key;
 	for (i = 1; i < n; i++) {
 		key = arr[i];
 		j = i - 1;
-		while (j >= 0 &&  arr[j] > key) {
-			arr[j+1] = arr[j];
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
 			j = j - 1;
 		}
-		arr[j+1] = key;
+		arr[j + 1] = key;
 	}
 }
 
@@ -26,6 +26,7 @@ void printArray(int arr[], int n) {
 int main() {
 	int arr[] = {12, 11, 13, 5, 6};
 	int n = sizeof(arr) / sizeof(arr[0]);
+	printArray(arr, n);
 	insertionSort(arr, n);
 	printArray(arr, n);
 	return 0;
