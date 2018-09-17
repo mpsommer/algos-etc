0b4bd18e43d14e71bc39d9034944a777864697a8
Michael Sommer <mps@mindsharemed.com>
2018-09-17 06:43:15 -0700

    Created brute force solution to the maximum sub array problem.

---
+++ maximum-subarray/max-sub-brute-force.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Input: [-2,1,-3,4,-1,2,1,-5,4],
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+
+int* maxSubArray(int* arr, int size) {
+	int i, j, maxSum, maxSoFar;
+	static int resultArr[3] = {-1,-1,-1};
+	maxSum = 0;
+	for (i = 0; i < size; i++) {
+		maxSoFar = 0;
+		for (j = i; j < size; j++) {
+			maxSoFar += arr[j];
+			if (maxSoFar > maxSum) {
+				maxSum = maxSoFar;
+				resultArr[0] = i;
+				resultArr[1] = j;
+				resultArr[2] = maxSum;
+			}
+		}
+	}
+	return resultArr;
+}
+
+
+void printArray(int arr[], int size) {
+
+	int i;
+	for (i = 0; i < size; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+int main()
+{
+	int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+	int arr_size = sizeof(arr)/sizeof(arr[0]);
+
+	printf("Given array is \n");
+	printArray(arr, arr_size);
+
+	int* result = maxSubArray(arr, arr_size);
+
+	printf("Max subarray start index: %d \n", result[0]);
+	printf("Max subarray stop index: %d \n", result[1]);
+	printf("Max subarray sum: %d \n", result[2]);
+	return 0;
+}
