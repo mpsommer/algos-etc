869725acca3d291b92ade862e682df5140b38b7c
Michael Sommer <mps@mindsharemed.com>
2018-10-01 06:19:57 -0700

    Implemented counting sort.

--- sorting/counting-sort.cpp
+++ sorting/counting-sort.cpp
@@ -1,45 +1,83 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // https://www.geeksforgeeks.org/counting-sort/
 
+void printVec(vector<int> vec)
+{
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << " ";
+	}
+	cout << endl;
+}
 
-int findMaxValue(int arr[], int arrLength) {
+int findMaxValue(vector<int> vec)
+{
 	int maxValue = -1;
-	for (int i = 0; i < arrLength; i++) {
-		if (maxValue < arr[i]) {
-			maxValue = arr[i];
+	for (int i = 0; i < vec.size(); i++)
+	{
+		int value = vec[i] + 1;
+		if (maxValue < value)
+		{
+			maxValue = value;
 		}
 	}
 	return maxValue;
 }
 
-void countingSort(int* arr, int arrLength) {
-
-	int maxValue = findMaxValue(arr, arrLength);
-}
-
-void printArray(int A[], int size)
+vector<int> countingSort(vector<int> vec)
 {
-	int i;
-	for (i=0; i < size; i++)
-		printf("%d ", A[i]);
-	printf("\n");
+	int countLength = findMaxValue(vec);
+	vector<int> countVec(countLength);
+	int vecLength = vec.size();
+	vector<int> sortedVec(vecLength);
+	int i = {0};
+
+	// Puts the number of input elements equal to i
+	// for each integer i = 0, 1,..., countLength into countVec.
+	for (i = 0; i < vecLength; i++)
+	{
+		countVec[vec[i]] = countVec[vec[i]] + 1;
+	}
+
+	// Determines for each i = 0, 1, ..., countLength how many input
+	// elements are less than or equal to i by keeping a running sum
+	// of the vec countVec
+	for (i = 1; i < countLength; i++)
+	{
+		countVec[i] = countVec[i] + countVec[i - 1];
+	}
+
+	// Places each element vec[i] into its correct sorted position in the
+	// sortedVec.
+	for (i = vecLength - 1; i > 0; i--)
+	{
+		sortedVec[countVec[vec[i]] - 1] = vec[i];
+		countVec[vec[i]] = countVec[vec[i]] - 1;
+	}
+
+	return sortedVec;
 }
 
 /* Driver program to test above functions */
 int main()
 {
-	int arr[] = {12, 11, 13, 5, 6, 7};
-	int arr_size = sizeof(arr)/sizeof(arr[0]);
+	vector<int> vec;
+	vec.push_back(2);
+	vec.push_back(5);
+	vec.push_back(3);
+	vec.push_back(0);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(0);
+	vec.push_back(3);
 
-	printf("Given array is \n");
-	printArray(arr, arr_size);
+	cout << "Given array: " << endl;
+	printVec(vec);
+	vec = countingSort(vec);
 
-	cout << findMaxValue(arr, arr_size) << endl;
-
-	printf("\nSorted array is \n");
-	printArray(arr, arr_size);
-	return 0;
+	cout << "Sorted array: " << endl;
+	printVec(vec);
 }
-
