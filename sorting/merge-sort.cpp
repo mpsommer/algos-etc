cd0d4d4fe26676df10399c7e20c3b8d0e7e678e6
Michael Sommer <mps@mindsharemed.com>
2018-10-01 20:50:06 -0700

    Modified merge sort.

--- sorting/merge-sort.cpp
+++ sorting/merge-sort.cpp
@@ -1,75 +1,100 @@
 // Reference: https://www.geeksforgeeks.org/merge-sort/
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <iostream>
+#include <vector>
 
-void merge(int* arr, int l, int m, int r) {
-	int i, j, k;
+template<typename T>
+void merge(std::vector<T>& vec, int l, int m, int r)
+{
+	int i{0};
+	int j{0};
+	int k{0};
 	int leftLength = m + 1 - l;
 	int rightLength = r - m;
-	int left[leftLength], right[rightLength];
+	std::vector<T> left;
+	left.reserve(leftLength);
+	std::vector<T> right;
+	right.reserve(rightLength);
 
-	for (i = 0; i < leftLength; i++) {
-		left[i] = arr[l + i];
+	for (i = 0; i < leftLength; ++i)
+	{
+		left[i] = vec[l + i];
 	}
 
-	for (j = 0; j < rightLength; j++) {
-		right[j] = arr[m + 1 + j];
+	for (j = 0; j < rightLength; ++j)
+	{
+		right[j] = vec[m + 1 + j];
 	}
 
 	i = 0;
 	j = 0;
 	k = l;
 
-	while (i < leftLength && j < rightLength) {
-		if(left[i] <= right[j]) {
-			arr[k] = left[i++];
-		} else {
-			arr[k] = right[j++];
+	while (i < leftLength && j < rightLength)
+	{
+		if (left[i] <= right[j])
+		{
+			vec[k] = left[i++];
+		}
+		else
+		{
+			vec[k] = right[j++];
 		}
 		k++;
 	}
 
-	while (i < leftLength) {
-		arr[k++] = left[i++];
+	while (i < leftLength)
+	{
+		vec[k++] = left[i++];
 	}
 
-	while (j < rightLength) {
-		arr[k++] = right[j++];
+	while (j < rightLength)
+	{
+		vec[k++] = right[j++];
 	}
 }
 
-void mergeSort(int* arr, int l, int r) {
-	if (l < r) {
-		int m = l + (r - l) / 2;
-		mergeSort(arr, l , m);
-		mergeSort(arr, m + 1, r);
-		merge(arr, l, m, r);
-	} 
-}
-
-/* UTILITY FUNCTIONS */
-/* Function to print an array */
-void printArray(int A[], int size)
+template<typename T>
+void mergeSort(std::vector<T>& vec, int l, int r)
 {
-	int i;
-	for (i=0; i < size; i++)
-		printf("%d ", A[i]);
-	printf("\n");
+	if (l < r)
+	{
+		int m = l + (r - l) / 2;
+		mergeSort(vec, l, m);
+		mergeSort(vec, m + 1, r);
+		merge(vec, l, m, r);
+	}
+}
+
+template<typename T>
+void printVec(std::vector<T> vec)
+{
+	for (auto const& elem: vec)
+	{
+		std::cout << elem << " ";
+	}
+	std::cout << "\n";
 }
 
 /* Driver program to test above functions */
 int main()
 {
-	int arr[] = {12, 11, 13, 5, 6, 7};
-	int arr_size = sizeof(arr)/sizeof(arr[0]);
+	std::vector<int> vec;
+	vec.push_back(12);
+	vec.push_back(11);
+	vec.push_back(13);
+	vec.push_back(5);
+	vec.push_back(6);
+	// std::vector<char> vec;
+	// vec.push_back('r');
+	// vec.push_back('f');
+	// vec.push_back('u');
+	// vec.push_back('c');
+	// vec.push_back('A');
 
-	printf("Given array is \n");
-	printArray(arr, arr_size);
-
-	mergeSort(arr, 0, arr_size - 1);
-
-	printf("\nSorted array is \n");
-	printArray(arr, arr_size);
-	return 0;
+	std::cout << "Given array: " << "\n";
+	printVec(vec);
+	mergeSort(vec, 0, vec.size() - 1);
+	std::cout << "Sorted array: " << "\n";
+	printVec(vec);
 }
