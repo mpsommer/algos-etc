640d268b0b842296970cdc869ecb5c4f8e658d84
Michael Sommer <mps@mindsharemed.com>
2018-10-02 09:06:01 -0700

    Cleaned up file./

--- maps/stl-map.cpp
+++ maps/stl-map.cpp
@@ -1,91 +1,84 @@
 // ref: https://www.geeksforgeeks.org/map-associative-containers-the-c-standard-template-library-stl/
-// clang++ -std=c++11 -stdlib=libc++ -Weverything -Wno-c++98-compat-pedantic stl-map.cpp -o stlmap
 
 #include <iostream>
 #include <map>
 #include <iterator>
- 
-using namespace std;
- 
-int main()
-{
-    map <int, int> gquiz1;        // empty map container
- 
-    // pair insert(keyvalue,mapvalue) – Adds a new element to the map
-    gquiz1.insert(pair <int, int> (1, 40));
-    gquiz1.insert(pair <int, int> (2, 30));
-    gquiz1.insert(pair <int, int> (3, 60));
-    gquiz1.insert(pair <int, int> (4, 20));
-    gquiz1.insert(pair <int, int> (5, 50));
-    gquiz1.insert(pair <int, int> (6, 50)); 
-    gquiz1.insert(pair <int, int> (7, 10));
- 
-    // printing map gquiz1
-    map <int, int> :: iterator itr;
-    cout << "\nThe map gquiz1 is : \n";
-    cout << "\tKEY\tELEMENT\n";
+
+template<typename T>
+void printMap(std::map<T, T>& hashMap) {
+	// printing map hashMap
+	std::map<int, int>::iterator itr;
+	std::cout << "The map hashMap is : \n";
+	std::cout << "\tKEY\tELEMENT\n";
 	// begin() – Returns an iterator to the first element in the map
 	// end() – Returns an iterator to the theoretical element that follows last element in the map
-    for (itr = gquiz1.begin(); itr != gquiz1.end(); ++itr)
-    {
-        cout  <<  '\t' << itr->first 
-              <<  '\t' << itr->second << '\n';
-    }
-    cout << endl;
- 
-    // assigning the elements from gquiz1 to gquiz2
-    map <int, int> gquiz2(gquiz1.begin(), gquiz1.end());
- 
-    // print all elements of the map gquiz2
-    cout << "\nThe map gquiz2 after assign from gquiz1 is : \n";
-    cout << "\tKEY\tELEMENT\n";
-    for (itr = gquiz2.begin(); itr != gquiz2.end(); ++itr)
-    {
-        cout  <<  '\t' << itr -> first 
-              <<  '\t' << itr->second << '\n';
-    }
-    cout << endl;
- 
-    // remove all elements up to element with key=3 in gquiz2
-    cout << "\ngquiz2 after removal of elements less than key=3 : \n";
-    cout << "\tKEY\tELEMENT\n";
+	for (itr = hashMap.begin(); itr != hashMap.end(); ++itr)
+	{
+		std::cout << '\t' << itr->first
+			 << '\t' << itr->second << '\n';
+	}
+	std::cout << '\n';
+}
+
+
+int main()
+{
+	std::map<int, int> hashMap; // empty map container
+
+	// pair insert(keyvalue,mapvalue) – Adds a new element to the map
+	hashMap.insert(std::pair<int, int>(1, 40));
+	hashMap.insert(std::pair<int, int>(2, 30));
+	hashMap.insert(std::pair<int, int>(3, 60));
+	hashMap.insert(std::pair<int, int>(4, 20));
+	hashMap.insert(std::pair<int, int>(5, 50));
+	hashMap.insert(std::pair<int, int>(6, 50));
+	hashMap.insert(std::pair<int, int>(7, 10));
+
+	printMap(hashMap);
+
+	// assigning the elements from hashMap to gquiz2
+	std::map<int, int> gquiz2(hashMap.begin(), hashMap.end());
+
+	// // print all elements of the map gquiz2
+	std::cout << "\nThe map gquiz2 after assign from hashMap is : \n";
+
+	printMap(gquiz2);
+
+	// remove all elements up to element with key=3 in gquiz2
+	std::cout << "\ngquiz2 after removal of elements less than key=3 : \n";
+	std::cout << "\tKEY\tELEMENT\n";
 	// erase(iterator position) – Removes the element at the position pointed by the iterator
-    gquiz2.erase(gquiz2.begin(), gquiz2.find(3));
-    for (itr = gquiz2.begin(); itr != gquiz2.end(); ++itr)
-    {
-        cout  <<  '\t' << itr->first 
-              <<  '\t' << itr->second << '\n';
-    }
- 
-    // remove all elements with key = 4
-    int num;
+	gquiz2.erase(gquiz2.begin(), gquiz2.find(3));
+
+	printMap(gquiz2);
+
+	// remove all elements with key = 4
+	int num;
 	// erase(const g)- Removes the key value ‘g’ from the map
-    num = gquiz2.erase (4);
-    cout << "\ngquiz2.erase(4) : ";
-    cout << num << " removed \n" ;
-    cout << "\tKEY\tELEMENT\n";
-    for (itr = gquiz2.begin(); itr != gquiz2.end(); ++itr)
-    {
-        cout  <<  '\t' << itr->first 
-              <<  '\t' << itr->second << '\n';
-    }
- 
-    cout << endl;
- 
-    //lower bound and upper bound for map gquiz1 key = 5
-    cout << "gquiz1.lower_bound(5) : " << "\tKEY = ";
+	num = gquiz2.erase(4);
+	std::cout << "\ngquiz2.erase(4) : ";
+	std::cout << num << " removed \n";
+	std::cout << "\tKEY\tELEMENT\n";
+
+	printMap(gquiz2);
+
+	std::cout << '\n';
+
+	//lower bound and upper bound for map hashMap key = 5
+	std::cout << "hashMap.lower_bound(5) : "
+		 << "\tKEY = ";
 	// lower_bound(const g) – Returns an iterator to the first element that is
-	//  equivalent to mapped value with key value ‘g’ or definitely will not go 
+	//  equivalent to mapped value with key value ‘g’ or definitely will not go
 	//  before the element with key value ‘g’ in the map
-    cout << gquiz1.lower_bound(5)->first << '\t';
-    cout << "\tELEMENT = " << gquiz1.lower_bound(5)->second << endl;
-    cout << "gquiz1.upper_bound(5) : " << "\tKEY = ";
-	// upper_bound(const g) – Returns an iterator to the first element that is 
-	// equivalent to mapped value with key value ‘g’ or definitely will go after 
+	std::cout << hashMap.lower_bound(5)->first << '\t';
+	std::cout << "\tELEMENT = " << hashMap.lower_bound(5)->second << '\n';
+	std::cout << "hashMap.upper_bound(5) : "
+		 << "\tKEY = ";
+	// upper_bound(const g) – Returns an iterator to the first element that is
+	// equivalent to mapped value with key value ‘g’ or definitely will go after
 	// the element with key value ‘g’ in the map
-    cout << gquiz1.upper_bound(5)->first << '\t';
-    cout << "\tELEMENT = " << gquiz1.upper_bound(5)->second << endl;
-     
-    return 0;
- 
+	std::cout << hashMap.upper_bound(5)->first << '\t';
+	std::cout << "\tELEMENT = " << hashMap.upper_bound(5)->second << '\n';
+
+	return 0;
 }
